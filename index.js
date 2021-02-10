30daa52fa5828dcc1134168be6af27683bf2c1fe
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-02-10 12:52:04 -0500

    commit tree 5a7fdc7da7aa56e2a5ae82ba5daa27a7a3d07bb4

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Your code here
+class Polygon {
+  constructor(arr) {
+    this.sides = arr;
+  }
+  get countSides() {
+    return this.sides.length;
+  }
+
+  get perimeter() {
+    return this.sides.reduce((total, num) => total + num);
+  }
+}
+
+class Triangle extends Polygon {
+  get isValid() {
+    if (this.countSides === 3) {
+      if (
+        this.sides[0] + this.sides[1] > this.sides[2] &&
+        this.sides[1] + this.sides[2] > this.sides[0] &&
+        this.sides[2] + this.sides[1] > this.sides[0]
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+}
+
+class Square extends Polygon {
+  get isValid() {
+    if (
+      this.sides.every((x) => {
+        return x === this.sides[0];
+      })
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  get area() {
+    return this.sides[0] * this.sides[1];
+  }
+}
